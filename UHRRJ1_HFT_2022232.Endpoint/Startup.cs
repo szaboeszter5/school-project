f5bdd733bab78805e0cfebda591552da658d10fa
szaboeszteringeborg@stud.uni-obuda.hu <szaboeszteringeborg@stud.uni-obuda.hu>
2023-04-26 20:08:26 +0200

    endpoint created

--- UHRRJ1_HFT_2022232.Endpoint/Startup.cs
+++ UHRRJ1_HFT_2022232.Endpoint/Startup.cs
@@ -1,21 +1,51 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UHRRJ1_HFT_2022232.Logic;
+using UHRRJ1_HFT_2022232.Logic.Interfaces;
+using UHRRJ1_HFT_2022232.Models;
+using UHRRJ1_HFT_2022232.Repository;
 
 namespace UHRRJ1_HFT_2022232.Endpoint
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<MovieDbContext>();
+
+            services.AddTransient<IRepository<Movie>, MovieRepository>();
+            services.AddTransient<IRepository<Role>, RoleRepository>();
+            services.AddTransient<IRepository<Actor>, ActorRepository>();
+            services.AddTransient<IRepository<Director>, DirectorRepository>();
+
+            services.AddTransient<IMovieLogic, MovieLogic>();
+            services.AddTransient<IRoleLogic, RoleLogic>();
+            services.AddTransient<IActorLogic, ActorLogic>();
+            services.AddTransient<IDirectorLogic, DirectorLogic>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "UHRRJ1_HFT_2022232.Endpoint", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,16 +54,17 @@ namespace UHRRJ1_HFT_2022232.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "UHRRJ1_HFT_2022232.Endpoint v1"));
             }
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
