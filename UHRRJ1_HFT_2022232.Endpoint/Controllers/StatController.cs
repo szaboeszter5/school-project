3473bff9b7a0bc0e3377ba7e0e8e9a1327d11949
szaboeszteringeborg@stud.uni-obuda.hu <szaboeszteringeborg@stud.uni-obuda.hu>
2023-04-26 20:29:55 +0200

    statcontroller added

---
+++ UHRRJ1_HFT_2022232.Endpoint/Controllers/StatController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using UHRRJ1_HFT_2022232.Logic.Interfaces;
+using UHRRJ1_HFT_2022232.Logic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UHRRJ1_HFT_2022232.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class StatController : ControllerBase
+    {
+        IMovieLogic logic;
+
+        public StatController(IMovieLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet("{year}")]
+        public double? AverageRatePerYear(int year)
+        {
+            return this.logic.GetAverageRatePerYear(year);
+        }
+
+        [HttpGet]
+        public IEnumerable<MovieLogic.YearInfo> YearStatistics(int year)
+        {
+            return this.logic.YearStatistics();
+        }
+    }
+}
