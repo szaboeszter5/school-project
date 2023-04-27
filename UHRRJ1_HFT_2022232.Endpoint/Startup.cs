cc4a8d46fd9048cb01623599273c262a59ed7953
szaboeszteringeborg@stud.uni-obuda.hu <szaboeszteringeborg@stud.uni-obuda.hu>
2023-04-27 10:50:00 +0200

    exception handling

--- UHRRJ1_HFT_2022232.Endpoint/Startup.cs
+++ UHRRJ1_HFT_2022232.Endpoint/Startup.cs
@@ -1,15 +1,11 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using UHRRJ1_HFT_2022232.Logic;
 using UHRRJ1_HFT_2022232.Logic.Interfaces;
 using UHRRJ1_HFT_2022232.Models;
@@ -57,6 +53,13 @@ namespace UHRRJ1_HFT_2022232.Endpoint
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "UHRRJ1_HFT_2022232.Endpoint v1"));
             }
+
+            app.UseExceptionHandler(x => x.Run(async context =>
+            {
+                var exc = context.Features.Get<IExceptionHandlerPathFeature>().Error;
+                var response = new { Msg = exc.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
 
             app.UseRouting();
 
