a0999197937dead88344dfff171c7aa60ea209c7
szaboeszteringeborg@stud.uni-obuda.hu <szaboeszteringeborg@stud.uni-obuda.hu>
2023-04-30 12:33:47 +0200

    reflection exception in model repos fixed

--- UHRRJ1_HFT_2022232.Repository/ModelRepositories/ActorRepository.cs
+++ UHRRJ1_HFT_2022232.Repository/ModelRepositories/ActorRepository.cs
@@ -19,7 +19,10 @@ namespace UHRRJ1_HFT_2022232.Repository
             var old = Read(item.ActorId);
             foreach (var prop in old.GetType().GetProperties())
             {
-                prop.SetValue(old, prop.GetValue(item));
+                if (prop.GetAccessors().FirstOrDefault(t => t.IsVirtual) == null)
+                {
+                    prop.SetValue(old, prop.GetValue(item));
+                }
             }
             ctx.SaveChanges();
         }
