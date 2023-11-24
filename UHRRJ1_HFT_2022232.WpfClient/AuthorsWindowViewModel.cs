5f4e5538b8c365a3972ce240ac7e5410cce9b195
szaboeszteringeborg@stud.uni-obuda.hu <szaboeszteringeborg@stud.uni-obuda.hu>
2023-11-24 05:11:54 +0100

    author editor set up

---
+++ UHRRJ1_HFT_2022232.WpfClient/AuthorsWindowViewModel.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using UHRRJ1_HFT_2022232.Models;
+
+namespace UHRRJ1_HFT_2022232.WpfClient
+{
+    public class AuthorsWindowViewModel : ObservableRecipient
+    {
+        private string errorMessage;
+        public string ErrorMessage
+        {
+            get { return errorMessage; }
+            set { SetProperty(ref errorMessage, value); }
+        }
+
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
+
+        public RestCollection<Author> Authors { get; set; }
+
+        private Author selectedAuthor;
+        public Author SelectedAuthor
+        {
+            get { return selectedAuthor; }
+            set
+            {
+                if (value != null)
+                {
+                    selectedAuthor = new Author()
+                    {
+                        AuthorId = value.AuthorId,
+                        AuthorName = value.AuthorName,
+                        Books = value.Books
+                    };
+
+                    OnPropertyChanged();
+                    (DeleteAuthorCommand as RelayCommand).NotifyCanExecuteChanged();
+                    (UpdateAuthorCommand as RelayCommand).NotifyCanExecuteChanged();
+                }
+            }
+        }
+
+        public ICommand CreateAuthorCommand { get; set; }
+        public ICommand DeleteAuthorCommand { get; set; }
+        public ICommand UpdateAuthorCommand { get; set; }
+        public ICommand ListByNumberOfBooksCommand { get; set; }
+
+        public AuthorsWindowViewModel()
+        {
+            if (!IsInDesignMode)
+            {
+                Authors = new RestCollection<Author>("http://localhost:23125/", "Author", "hub");
+
+                CreateAuthorCommand = new RelayCommand(() =>
+                {
+                    Authors.Add(new Author()
+                    {
+                        AuthorName = SelectedAuthor.AuthorName
+                    });
+                    System.Threading.Thread.Sleep(150);
+                    Authors.Update(Authors.Last());
+                });
+
+                UpdateAuthorCommand = new RelayCommand(() =>
+                {
+                    try
+                    {
+                        Authors.Update(SelectedAuthor);
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        ErrorMessage = ex.Message;
+                    }
+
+                });
+
+                DeleteAuthorCommand = new RelayCommand(() =>
+                {
+                    Authors.Delete(SelectedAuthor.AuthorId);
+                },
+                () =>
+                {
+                    return SelectedAuthor != null;
+                });
+
+                SelectedAuthor = new Author();
+            }
+        }
+    }
+}
